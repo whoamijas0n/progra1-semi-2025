eb358dd0f6208e33362cb68ed299c7cf05ffe536
Herber Marin <juniorserpasyt@gmail.com>
2025-07-24 13:47:44 -0600

    Creando mi primer proyecto en C# y un Hola mundo

---
+++ MiPrimerProyectoC#/MiPrimerProyectoC#/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace MiPrimerProyectoC_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHolaMundo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHolaMundo
+            // 
+            this.lblHolaMundo.AutoSize = true;
+            this.lblHolaMundo.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHolaMundo.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblHolaMundo.Location = new System.Drawing.Point(154, 159);
+            this.lblHolaMundo.Name = "lblHolaMundo";
+            this.lblHolaMundo.Size = new System.Drawing.Size(177, 33);
+            this.lblHolaMundo.TabIndex = 0;
+            this.lblHolaMundo.Text = "Hola Mundo";
+            this.lblHolaMundo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHolaMundo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHolaMundo;
+    }
+}
+

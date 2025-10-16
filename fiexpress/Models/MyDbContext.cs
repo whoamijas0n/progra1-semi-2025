6463c7c06b7ef426a1a750d2d618f8490721831a
rousemary <rousemary@DESKTOP-5AD1KOK>
2025-10-15 21:28:57 -0600

    se corrigio unos errores y se creo la tabla  rol

---
+++ fiexpress/Models/MyDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace fiexpress.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() { }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Aqui vamos a agregar los modelos de las entidades que creemos para cada tabla
+            //Un ejemplo;
+            //modelBuilder.Entity<nombre de la calse>().HasKey(a => a.idDeLaTabla);
+
+            //EJEMPLO PARA LA TABLA ROL
+            modelBuilder.Entity<Rol>().HasKey(a => a.idRol);
+
+
+        }
+
+    }
+}
---
+++ fiexpress/Models/Rol.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace fiexpress.Models
+{
+    public class Rol
+    {
+        [Key]
+        public int idRol { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public bool activo { get; set; }
+    }
+}

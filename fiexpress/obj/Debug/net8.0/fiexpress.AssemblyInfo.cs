de7e1d16ba08b4d928b42b1940b50214d132d099
rousemary <rousemary@DESKTOP-5AD1KOK>
2025-11-23 13:52:08 -0600

    se agregaron pantallas de carga, se esta configurando nuevos estilos

--- fiexpress/obj/Debug/net8.0/fiexpress.AssemblyInfo.cs
+++ fiexpress/obj/Debug/net8.0/fiexpress.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fiexpress")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3097a5273c69a1764a8d555c1dcc1bf23253539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d0de112ca50f9f7e7556e1fafebe6702d5dcd34")]
 [assembly: System.Reflection.AssemblyProductAttribute("fiexpress")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fiexpress")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

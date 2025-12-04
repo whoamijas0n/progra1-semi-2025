03a4380f2ebdbb897188985ab43594cefb109712
rousemary <rousemary@DESKTOP-5AD1KOK>
2025-12-04 13:10:22 -0600

    Se subieron todos los archivos restantes para la finalizacion de la aplicacion web de programacion computacional

--- webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
+++ webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+357a40fa1eca0d2944b8bc7958d9871e4c9aaeea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a4bf48f76a485f3851f3fb304c81b13ab7eda07")]
 [assembly: System.Reflection.AssemblyProductAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
